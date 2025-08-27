e3fc0339a4c748c75a312ce1be47d040c9b86eb1
GabrielPercincula <141972349+GabrielPercincula@users.noreply.github.com>
2025-08-27 08:36:34 -0300

    Add files via upload

---
+++ compare-the-triplets.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete a função 'compareTriplets' abaixo.
+ *
+ * A função deve retornar um ARRAY DE INTEIROS.
+ * A função aceita os seguintes parâmetros:
+ * 1. ARRAY DE INTEIROS a
+ * 2. ARRAY DE INTEIROS b
+ */
+
+function compareTriplets(a, b) {
+    let alicePoints = 0;
+    let bobPoints = 0;
+
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] > b[i]) {
+            alicePoints++;
+        } else if (a[i] < b[i]) {
+            bobPoints++;
+        }
+    }
+
+    return [alicePoints, bobPoints];
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const b = readLine().replace(/\s+$/g, '').split(' ').map(bTemp => parseInt(bTemp, 10));
+
+    const result = compareTriplets(a, b);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
